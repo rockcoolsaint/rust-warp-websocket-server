9c6e8875ae505043861df89926c4a3a6dabf69a8
Innocent Onyemaenu <rockcoolsaint@gmail.com>
2025-03-13 13:12:00 +0100

    updating handlers.rs and ws.rs

--- src/handlers.rs
+++ src/handlers.rs
@@ -5,5 +5,5 @@ use crate::{ws, Clients, Result};
 pub async fn ws_handler(ws: warp::ws::Ws, clients: Clients ) -> Result<impl Reply> {
   println!("ws_handler");
 
-  Ok(ws.on_upgrade(move |socket| ws::client_connection(socket)))
+  Ok(ws.on_upgrade(move |socket| ws::client_connection(socket, clients)))
 }
